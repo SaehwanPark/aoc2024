7f89714f0f2bc567f3fc61908cb1fe2671890d33
Sae-Hwan Park <sae-hwan.park@pennmedicine.upenn.edu>
2025-07-30 21:22:09 -0400

    refactor day 3

--- examples/day03.rs
+++ examples/day03.rs
@@ -1,7 +1,8 @@
+use anyhow::Result;
 use regex::Regex;
 use std::fs;
 
-fn solve_part1(input: &str) -> i32 {
+fn calculate_sumproduct(input: &str) -> i32 {
   // pattern to match valid mul(X,Y) instruction where X,Y are 1-3 digits
   let mul_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("Failed to compile regex");
 
@@ -16,7 +17,7 @@ fn solve_part1(input: &str) -> i32 {
   total
 }
 
-fn solve_part2(input: &str) -> i32 {
+fn calculate_sumproduct_with_instruction(input: &str) -> i32 {
   let instruction_regex = Regex::new(r"(?:mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))")
     .expect("Failed to compile regex");
   let mut total = 0;
@@ -46,10 +47,23 @@ fn solve_part2(input: &str) -> i32 {
   total
 }
 
-fn main() {
-  let input = fs::read_to_string("input/day03_full.txt").expect("Failed to read input file");
-  let part1_result = solve_part1(&input);
-  let part2_result = solve_part2(&input);
-  println!("Part 1 Answer = {part1_result}");
-  println!("Part 2 Answer = {part2_result}");
+fn solve(input: &str, part: u8) -> i32 {
+  match part {
+    1 => calculate_sumproduct(input),
+    2 => calculate_sumproduct_with_instruction(input),
+    _ => panic!("Only parts 1 or 2."),
+  }
+}
+
+fn print_result(filepath: &str, puzzle_kind: &str) -> Result<()> {
+  let input = fs::read_to_string(filepath)?;
+  println!("Input: {puzzle_kind}");
+  println!("Part 1 result = {}", solve(&input, 1));
+  println!("Part 2 result = {}\n", solve(&input, 2));
+  Ok(())
+}
+
+fn main() -> Result<()> {
+  print_result("input/day03_full.txt", "Full puzzle")?;
+  Ok(())
 }
